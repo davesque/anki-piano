06693ea38b397bced9246669b8e06830812c6739
David Sanders <davesque@gmail.com>
2016-01-16 15:35:54 -0700

    Update make-pngs.sh

--- bin/make-pngs.sh
+++ bin/make-pngs.sh
@@ -3,5 +3,5 @@
 mkdir -p pngs
 
 for pdf in pdfs/*; do
-  gs -dNOPAUSE -dBATCH -sDEVICE=pngalpha -r300 -sOutputFile="pngs/$(basename $pdf | cut -d. -f1).png" "$pdf"
+  gs -dNOPAUSE -dBATCH -sDEVICE=pngalpha -r400 -sOutputFile="pngs/$(basename $pdf | cut -d. -f1).png" "$pdf"
 done
